788920a4d7e9e9a423a420d182452247ec46e930
Leon Suarez <leonsua@gmail.com>
2022-02-07 18:59:25 -0500

    run renders without errors test on NumberPlayers and see it fail

--- src/components/number-players/NumberPlayers.jsx
+++ src/components/number-players/NumberPlayers.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const NumberPlayers = () => {
   return(
     null
   );
 };
+
+NumberPlayers.propTypes = {
+  numPlayers: PropTypes.number.isRequired
+}
--- src/components/number-players/NumberPlayers.test.jsx
+++ src/components/number-players/NumberPlayers.test.jsx
@@ -1,5 +1,43 @@
 import { shallow } from 'enzyme';
+import { NumberPlayers } from './NumberPlayers';
+import checkPropTypes from 'check-prop-types';
 
-test('renders without errors', () => {
-  
+const defaultProps = { numPlayers: 1 };
+
+/**
+ * Function to create a ShallowWrapper to the NumberPlayers component
+ * @function setup 
+ * @param {number} numPlayers 
+ * @returns ShallowWrapper
+ */
+
+const setup = (numPlayers) => {
+  const setupProps = {...defaultProps, ...numPlayers}
+  return shallow(<NumberPlayers {...setupProps} />)
+};
+
+test('does not throw an error with expected props', () => {
+  const propsError = checkPropTypes(NumberPlayers.propTypes, defaultProps, 'props', NumberPlayers.name);
+  expect(propsError).toBeUndefined();
+});
+
+describe('if there are no number of players selected', () => {
+  test('renders without errors', () => {
+    const wrapper = setup();
+    const component = wrapper.find('[data-test="number-players-component"]');
+    expect(component.length).toBe(1);
+  });
+  test('number players buttons disabled ones a number is picked', () => {
+
+  });
+  test('PlayersNames component is not show before select number of players', () => {
+
+  });
+  test('StartBubbon component is not show', () => {
+
+  });
+});
+
+describe('if there are number of players selected', () => {
+
 });
